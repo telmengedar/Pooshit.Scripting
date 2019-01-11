4f8423a10963191678658f5a8a664901909fde86
Toni Gottschall <toni.gottschall@nightlycode.de>
2019-01-11 03:49:29 +0100

    extended tests to custom parameter arrays

---
+++ Scripting.Tests/Data/Parameter.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Scripting.Tests.Data {
+
+    public class Parameter {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString() {
+            return $"{Name}={Value}";
+        }
+    }
+}
--- Scripting.Tests/MethodCallTests.cs
+++ Scripting.Tests/MethodCallTests.cs
@@ -2,7 +2,7 @@
 using NightlyCode.Scripting.Data;
 using NightlyCode.Scripting.Parser;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
+using Scripting.Tests.Data;
 
 namespace Scripting.Tests {
 
@@ -10,30 +10,34 @@ namespace Scripting.Tests {
     public class MethodCallTests {
         readonly IScriptParser parser = new ScriptParser();
 
-        public string MethodWithArrayParameters(params string[] parameters) {
-            return "success:"+string.Join(",", parameters);
+        public string MethodWithArrayParameters(string prefix, params Parameter[] parameters)
+        {
+            return $"{prefix}:{string.Join<Parameter>(",", parameters)}";
         }
 
-        public string Parameter(string value) {
-            return value;
+        public Parameter Parameter(string name, string value) {
+            return new Parameter {
+                Name = name,
+                Value = value
+            };
         }
 
         [Test, Parallelizable]
         public void CallMethodWithArrayParameters() {
-            IScript script = parser.Parse("$test.methodwitharrayparameters([$test.parameter(\"1\"),$test.parameter(\"7\")])", new Variable("test", this));
-            Assert.AreEqual("success:1,7", script.Execute());
+            IScript script = parser.Parse("$test.methodwitharrayparameters(\"success\", [$test.parameter(\"n\", \"1\"),$test.parameter(\"m\", \"7\")])", new Variable("test", this));
+            Assert.AreEqual("success:n=1,m=7", script.Execute());
         }
 
         [Test, Parallelizable]
         public void CallMethodWithParamsArray()
         {
-            IScript script = parser.Parse("$test.methodwitharrayparameters($test.parameter(\"1\"),$test.parameter(\"7\"))", new Variable("test", this));
-            Assert.AreEqual("success:1,7", script.Execute());
+            IScript script = parser.Parse("$test.methodwitharrayparameters(\"success\", $test.parameter(\"n\", \"1\"),$test.parameter(\"m\", \"7\"))", new Variable("test", this));
+            Assert.AreEqual("success:n=1,m=7", script.Execute());
         }
 
         [Test, Parallelizable]
         public void CallParamsArrayWithoutArguments() {
-            IScript script = parser.Parse("$test.methodwitharrayparameters()", new Variable("test", this));
+            IScript script = parser.Parse("$test.methodwitharrayparameters(\"success\")", new Variable("test", this));
             Assert.AreEqual("success:", script.Execute());
         }
     }
